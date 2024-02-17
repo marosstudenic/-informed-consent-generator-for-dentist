7407c254a0d3df35fbd75429ab7987f6bb39fd46
Maros Studenic <studenic.maros@gmail.com>
2024-02-17 17:27:22 +0000

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Alert } from "flowbite-react";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -18,7 +19,7 @@ export default function Home() {
               </a>
             </div>
             <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+              <Image src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
             </div>
           </div>
         </section>
@@ -26,13 +27,13 @@ export default function Home() {
         <section className="bg-white dark:bg-gray-900">
           <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn&apos;t reinvent the wheel</h2>
               <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
               <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
             </div>
             <div className="grid grid-cols-2 gap-4 mt-8">
-              <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
-              <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+              <Image className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+              <Image className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
             </div>
           </div>
         </section>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,6 @@
-export function Page() {
+import { PatientForm } from "../patients/create/PatientForm";
+
+export default function Page() {
     return (
         <div className='pt-10 container min-h-[80vh]'>
             <h1 className='text-lg mb-8'>Pridať nového pacienta</h1>
